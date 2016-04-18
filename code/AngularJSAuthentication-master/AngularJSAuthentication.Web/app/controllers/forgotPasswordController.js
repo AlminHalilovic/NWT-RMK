bf6b2cd16421555efae00efe6c4563fd3e1e5f92
Almin Halilović <ahalilovic5@etf.unsa.ba>
2016-04-18 22:43:18 +0200

    Potvrda registracije putem maila, izmjena passworda putem maila

---
+++ code/AngularJSAuthentication-master/AngularJSAuthentication.Web/app/controllers/forgotPasswordController.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+app.controller('forgotPasswordController', ['$scope', '$location', '$timeout', 'authService', function ($scope, $location, $timeout, authService) {
+
+    $scope.savedSuccessfully = false;
+    $scope.message = "";
+
+    $scope.userData = {
+        Email: ""
+    };
+
+    $scope.forgotPassword = function () {
+
+        console.log($scope.userData);
+        authService.forgotPassword($scope.userData).then(function (response) {
+
+            $scope.savedSuccessfully = true;
+            $scope.message = "Please check your email to reset your password.";
+            // startTimer();
+
+        },
+         function (response) {
+             var errors = [];
+             for (var key in response.data.modelState) {
+                 for (var i = 0; i < response.data.modelState[key].length; i++) {
+                     errors.push(response.data.modelState[key][i]);
+                 }
+             }
+             $scope.message = "Failed to send password reset link due to:" + errors.join(' ');
+         });
+    };
+
+    var startTimer = function () {
+        var timer = $timeout(function () {
+            $timeout.cancel(timer);
+            $location.path('/login');
+        }, 2000);
+    }
+
+}]);
